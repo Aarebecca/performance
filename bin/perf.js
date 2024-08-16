3ca4d3ffcf02c0c8f7c235cb718e775f28ca0374
Aaron <yangtao.yangtao@antgroup.com>
2024-08-16 19:36:36 +0800

    refactor: adapt monorepo, support launch without config

--- bin/perf.js
+++ bin/perf.js
@@ -2,31 +2,43 @@
 
 const fs = require('fs');
 const path = require('path');
-const { exec, spawn } = require('child_process');
+const { spawn } = require('child_process');
 require('../dist/setup.js');
 
-const configFilePath = path.join(process.cwd(), 'perf.config.js');
-const vitePath = path.join(process.cwd(), 'node_modules/.bin/vite');
+const userConfigPath = path.join(process.cwd(), 'perf.config.js');
+const vitePath = path.join(
+  path.dirname(require.resolve('vite')),
+  '/bin/vite.js',
+);
 
-if (fs.existsSync(configFilePath)) {
-  console.log('Start to run performance test...');
+let actualConfigPath = userConfigPath;
 
-  const child = spawn('node', [vitePath, '--config', configFilePath]);
+if (!fs.existsSync(userConfigPath)) {
+  actualConfigPath = path.join(
+    process.cwd(),
+    '/node_modules/.perf',
+    'perf.config.js',
+  );
 
-  child.stdout.on('data', (data) => {
-    const msg = `${data}`;
-    if (msg.startsWith('[perf]')) console.log(msg);
-  });
-
-  child.stderr.on('data', (data) => {
-    console.error(data.toString());
-  });
-
-  child.on('close', (code) => {
-    console.log(`\x1b[32mPerformance test done.\x1b[0m`);
-  });
-
-  // TODO 使用 playwright 打开浏览器
-} else {
-  console.log('perf.config.json does not exist');
+  fs.writeFileSync(
+    actualConfigPath,
+    `import { defineConfig } from 'perf';export default defineConfig({});`,
+  );
 }
+
+console.log('Start to run performance test...');
+
+const child = spawn('node', [vitePath, '--config', actualConfigPath]);
+
+child.stdout.on('data', (data) => {
+  const msg = `${data}`;
+  if (msg.startsWith('[perf]')) console.log(msg);
+});
+
+child.stderr.on('data', (data) => {
+  console.error(data.toString());
+});
+
+child.on('close', () => {
+  console.log(`\x1b[32mPerformance test done.\x1b[0m`);
+});
