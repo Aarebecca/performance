3ac992d7c0b2bd8c9610bf74cde2acc36cbb16b4
Aaron <yangtao.yangtao@antgroup.com>
2024-08-16 15:50:43 +0800

    refactor: abstract client runner

--- src/client/entry.ts
+++ src/client/entry.ts
@@ -1,7 +1,6 @@
-import { analyzeFrame, analyzeTime } from './analysis';
 import config from './config.json';
 import * as tests from './exports';
-import { Performance } from './performance';
+import { Runner } from './runner';
 import type { Test } from './types';
 
 const tasks = { ...tests } as Record<string, Test>;
@@ -10,7 +9,7 @@ function connect() {
   const port = (config as any).perf.socket.port;
 
   const ws = new WebSocket(`ws://localhost:${port}`);
-  const runner = new Runner();
+  const runner = new Runner(tasks);
 
   ws.onopen = () => {
     console.log('WebSocket connected on port:', port);
@@ -18,7 +17,7 @@ function connect() {
     ws.send(
       JSON.stringify({
         signal: 'ready',
-        list: Object.keys(tasks),
+        tasks: Object.keys(tasks),
         userAgent: navigator.userAgent,
       }),
     );
@@ -34,107 +33,13 @@ function connect() {
       ws.send(JSON.stringify({ signal: 'request' }));
     } else if (signal === 'complete') {
       ws.close();
-      window.close();
+      // window.close();
     } else if (signal === 'disconnect') {
       ws.close();
     }
   };
 
   return ws;
-}
-
-class Runner {
-  #config = {
-    epochs: 10,
-  };
-
-  #init() {
-    const root = document.getElementById('__test_root__')!;
-    root.innerHTML = '';
-  }
-
-  async execute(task: string) {
-    this.#init();
-    const test = tasks[task];
-
-    if (typeof test === 'function') {
-      return await this.iterate(task, test);
-    }
-
-    console.warn(`Test ${task} is unsupported, it will be skipped.`);
-    return { passed: false };
-  }
-
-  async iterate(name: string, callback: Test) {
-    const perf = new Performance();
-    const context = { perf };
-    const results = [];
-
-    const epochs = callback.epochs ?? this.#config.epochs;
-
-    console.log(`Start ${name}...`);
-    try {
-      for (let i = 0; i < epochs; i++) {
-        console.log(`Start ${name} in epoch: ${i}`);
-        await callback(context);
-        results.push(perf.export());
-      }
-    } catch (e) {
-      console.error(`Error in ${name}:`, e);
-    }
-    console.log(`Complete ${name}`);
-
-    return this.statistic(results);
-  }
-
-  statistic(records: any[]) {
-    console.log('statistic', records);
-    const results = {};
-
-    if (records.length === 0) return { passed: false };
-
-    const _analyzeTime = () => {
-      if (!records[0].time) return;
-      const timeKeys = Object.keys(records[0].time);
-
-      const time: Record<string, number[]> = {};
-
-      records.forEach((record) => {
-        timeKeys.forEach((key) => {
-          if (!time[key]) time[key] = [];
-          time[key].push(record.time[key]);
-        });
-      });
-
-      const timeResult = timeKeys.map((key) => {
-        const result = analyzeTime(time[key]);
-
-        if (!result.reliable) {
-          console.warn(`The evaluation of ${key} is too fast to be reliable.`);
-        }
-
-        Object.assign(result, { key });
-        return result;
-      });
-
-      Object.assign(results, { time: timeResult });
-    };
-
-    const _analyzeFrame = () => {
-      if (!records[0].frame) return;
-
-      const frameResults = records.map((record) => analyzeFrame(record.frame));
-      const sorted = frameResults.toSorted((a, b) => a.variance - b.variance);
-      Object.assign(results, { frame: sorted[0] });
-    };
-
-    _analyzeTime();
-    _analyzeFrame();
-
-    Object.assign(results, { passed: true });
-
-    return results;
-  }
 }
 
 connect();
---
+++ src/client/runner.ts
@@ -0,0 +1,95 @@
+import { analyzeFrame, analyzeTime } from './analyze';
+import { Performance } from './performance';
+import type { Test } from './types';
+
+export class Runner {
+  #init() {
+    const root = document.getElementById('__test_root__')!;
+    root.innerHTML = '';
+  }
+
+  constructor(private tasks: Record<string, Test>) {}
+
+  async execute(task: string) {
+    this.#init();
+    const test = this.tasks[task];
+
+    if (typeof test === 'function') {
+      return await this.iterate(task, test);
+    }
+
+    return { status: 'skipped' };
+  }
+
+  async iterate(name: string, callback: Test) {
+    const perf = new Performance();
+    const context = { perf };
+    const results = [];
+
+    const iterations = callback.iteration ?? 10;
+
+    console.log(`Start ${name}...`);
+    try {
+      for (let i = 0; i < iterations; i++) {
+        this.#init();
+        console.log(`Start ${name} in epoch: ${i}`);
+        await callback(context);
+        results.push(perf.export());
+      }
+    } catch (e) {
+      console.error(`Error in ${name}:`, e);
+      return { status: 'failed' };
+    }
+    console.log(`Complete ${name}`);
+
+    return this.statistic(results);
+  }
+
+  statistic(records: any[]) {
+    const results = {};
+
+    const _analyzeTime = () => {
+      if (!records[0].time) return;
+      const timeKeys = Object.keys(records[0].time);
+
+      const time: Record<string, number[]> = {};
+
+      records.forEach((record) => {
+        timeKeys.forEach((key) => {
+          if (!time[key]) time[key] = [];
+          time[key].push(record.time[key]);
+        });
+      });
+
+      const timeResult = timeKeys.map((key) => {
+        const result = analyzeTime(time[key]);
+
+        if (!result.reliable) {
+          console.warn(`The evaluation of ${key} is too fast to be reliable.`);
+        }
+
+        Object.assign(result, { key });
+        return result;
+      });
+
+      Object.assign(results, { time: timeResult });
+    };
+
+    const _analyzeFrame = () => {
+      if (!records[0].frame) return;
+
+      const frameResults = records.map((record) =>
+        analyzeFrame(record.frame.map((f: { time: number }) => f.time)),
+      );
+      const sorted = frameResults.toSorted((a, b) => a.variance - b.variance);
+      Object.assign(results, { frame: sorted[0] });
+    };
+
+    _analyzeTime();
+    _analyzeFrame();
+
+    Object.assign(results, { status: 'passed' });
+
+    return results;
+  }
+}
